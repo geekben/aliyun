76525c62a0bf31fc0e2234af0518938269147b78
jumping <quzhengping@gmail.com>
2013-03-12 17:41:34 +0800

    added all

---
+++ oss/oss/README.md
@@ -0,0 +1,4 @@
+History
+1. the version is OSS_Python_API_20121010,download from Python SDK开发包(2012-10-10):http://aliyun_portal_storage.oss.aliyuncs.com/oss_api/OSS_Python_API_20121010.tar.gz?spm=5176.383663.0.63.1TZLsE&file=OSS_Python_API_20121010.tar.gz
+
+2.
---
+++ oss/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: UTF8 -*-
+# 
+#   Jumping Qu @ BPO
+#
+#vim: ts=4 sts=4 et sw=4
+#
+import sys
+
+if sys.version_info <= (2, 4):
+    error = "ERROR: boto requires Python Version 2.5 or above...exiting."
+    print >> sys.stderr, error
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+setup(
+      name="oss",
+      version="20121010",
+      description="AliYun OSS module",
+      author="AliYun OSS",
+      url="http://www.aliyun.com",
+      license="LGPL",
+      packages= find_packages(),
+      scripts = ["bin/osscmd"],
+      )
+
